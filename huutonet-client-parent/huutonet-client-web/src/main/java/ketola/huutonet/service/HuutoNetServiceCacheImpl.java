c3223183702d95fc6f75a95d24b1eab851b8b057
ketola <sauli.ketola@mbnet.fi>
2013-12-15 19:05:51 +0200

    Modified the cached auction item service to populate the cache based on a timer instead of doing it on first request

--- huutonet-client-parent/huutonet-client-web/src/main/java/ketola/huutonet/service/HuutoNetServiceCacheImpl.java
+++ huutonet-client-parent/huutonet-client-web/src/main/java/ketola/huutonet/service/HuutoNetServiceCacheImpl.java
@@ -1,47 +1,50 @@
 package ketola.huutonet.service;
 
-import java.util.Date;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import ketola.huutonet.domain.HuutoNetItem;
 
 public class HuutoNetServiceCacheImpl
     implements HuutoNetService
 {
-    private static final long FIVE_MINUTES = 300000;
-
-    private Date queryTimeStamp;
+    private static final long FIVE_MINUTES = 1000 * 60 * 5;
 
     private List<HuutoNetItem> cache;
 
     private HuutoNetService wrappedHuutoNetService;
 
+    public HuutoNetServiceCacheImpl()
+    {
+
+    }
+
+    public void init()
+    {
+        createTimer();
+    }
+
+    private Timer createTimer()
+    {
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate( new TimerTask()
+        {
+
+            @Override
+            public void run()
+            {
+                cache = wrappedHuutoNetService.fetchHuutoNetItems();
+            }
+        }, 1000, FIVE_MINUTES );
+        return timer;
+    }
+
     @Override
     public List<HuutoNetItem> fetchHuutoNetItems()
     {
-        if ( cache == null || isCacheExpired() )
-        {
-            cache = wrappedHuutoNetService.fetchHuutoNetItems();
-            queryTimeStamp = newCacheTimestamp();
-        }
+
         return cache;
-    }
-
-    private boolean isCacheExpired()
-    {
-        if ( queryTimeStamp == null )
-            return true;
-
-        Date now = new Date();
-
-        long cacheAge = now.getTime() - queryTimeStamp.getTime();
-
-        return cacheAge > FIVE_MINUTES;
-    }
-
-    protected Date newCacheTimestamp()
-    {
-        return new Date();
     }
 
     public void setWrappedHuutoNetService( HuutoNetService wrappedHuutoNetService )
