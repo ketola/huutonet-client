92894745dd3a1c1a33667a76460c4411b3587a3e
ketola <sauli.ketola@mbnet.fi>
2013-12-16 18:23:49 +0200

    adjusted the css to work inside iframe

--- huutonet-client-parent/huutonet-client-web/src/main/webapp/js/controllers.js
+++ huutonet-client-parent/huutonet-client-web/src/main/webapp/js/controllers.js
@@ -4,7 +4,7 @@
 
 angular.module('huutoNetApp.controllers', []).
   controller('HuutoNetItemsCtrl', ['$scope','$http', function($scope, $http) {  
-	  var url = '/service/items/?callback=JSON_CALLBACK';
+	  var url = 'http://huutonet-ketola.rhcloud.com/service/items/?callback=JSON_CALLBACK';
 	    $http.jsonp(url).success(function(data) {
 	        $scope.items = data;
 	    });
