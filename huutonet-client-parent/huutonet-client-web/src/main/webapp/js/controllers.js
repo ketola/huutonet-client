616d0b2b7d33dc8adfba96df0f140dee6730437b
ketola <sauli.ketola@mbnet.fi>
2014-01-19 14:47:56 +0200

    added filters for auction type in the ui

--- huutonet-client-parent/huutonet-client-web/src/main/webapp/js/controllers.js
+++ huutonet-client-parent/huutonet-client-web/src/main/webapp/js/controllers.js
@@ -6,14 +6,16 @@ angular.module('huutoNetApp.controllers', []).
   controller('HuutoNetItemsCtrl', ['$scope', '$http', 'HuutoNet', function($scope, $http, HuutoNet) {
 	  $http({method: 'GET', url: '/service/itemids/'}).
 		success(function(data, status, headers, config) {
-		     $scope.items = []
+		    $scope.filters = {ALL : {}, AUCTION: {type : 'AUCTION'}, BUY_NOW: {type: 'BUY_NOW'} };
+		    $scope.itemsFilter = $scope.filters.ALL;
+		    
+			$scope.items = []
 			 for(var j = 0; j < data.length; j++){
 				 $scope.items.push({id: data[j]});
 			 }
 			 
 			 for(var i = 0; i < $scope.items.length; i++){
-				 	//alert($scope.items[i].id);
-				  $http({method: 'GET', url: '/service/items/'+$scope.items[i].id, params: {index: i}}).
+				 $http({method: 'GET', url: '/service/items/'+$scope.items[i].id, params: {index: i}}).
 				  		success(function(data, status, headers, config) {
 				  			$scope.items[config.params.index] = data;
 				  });
